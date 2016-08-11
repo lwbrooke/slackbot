0b5c338bdba8a2e496e61427496409b52ea4d4ac
Logan Brooke <logan.w.brooke@gmail.com>
2016-08-11 01:11:40 -0700

    send slack-consumable response

--- src/message_router.py
+++ src/message_router.py
@@ -4,5 +4,5 @@ import json
 
 class SlackMessageRouter:
     def on_post(self, req, resp):
-        resp.body = json.dumps({'message': 'hello, world!'})
+        resp.body = json.dumps({'text': 'hello, world!'})
         resp.status = falcon.HTTP_200
