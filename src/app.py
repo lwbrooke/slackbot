490035269723be1ee1599a2f6a7db3758c5234a2
Logan Brooke <logan.w.brooke@gmail.com>
2016-08-11 01:07:51 -0700

    cors headers

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from .message_router import SlackMessageRouter
+from falcon_cors import CORS
 import falcon
 
 
 def get_app():
-    app = falcon.API()
+    cors = CORS(allow_all_origins=True)
+
+    app = falcon.API(middleware=[cors.middleware])
 
     app.add_route('/api/slack/messagerouter', SlackMessageRouter())
 
