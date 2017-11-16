5663f8094d0fd196debb672a3174fbfb4b472d65
Logan Brooke <logan.w.brooke@gmail.com>
2017-11-15 23:28:20 -0800

    add FloatRange type

--- fanghorn/utils.py
+++ fanghorn/utils.py
@@ -21,3 +21,32 @@ def merge_dicts(a, b):
 
 
 path_type = click.Path(file_okay=False, exists=True, resolve_path=True)
+
+
+class FloatRange(click.ParamType):
+    name = 'float'
+
+    def __init__(self, min=None, max=None, clamp=False):
+        if min is not None and max is not None and min > max:
+            raise ValueError('min {} must be less than or equal to max {}'.format(min, max))
+
+        self._min = min
+        self._max = max
+        self._clamp = clamp
+
+    def convert(self, value, param, ctx):
+        try:
+            value = float(value)
+        except ValueError:
+            self.fail('{} is not a valid float'.format(value), param, ctx)
+
+        if self._clamp:
+            value = max(value, self._min) if self._min is not None else value
+            value = min(value, self._max) if self._max is not None else value
+        else:
+            if self._min is not None and value < self._min:
+                self.fail('{} must be greater than or equal to {}'.format(value, self._min), param, ctx)
+            if self._max is not None and value > self._max:
+                self.fail('{} must be less than or equal to {}'.format(value, self._max), param, ctx)
+
+        return value
